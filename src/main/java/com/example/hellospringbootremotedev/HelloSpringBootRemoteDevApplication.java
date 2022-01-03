1a0d82cb99390948ee6d6a59b48aff6fa0b208bd
Masaru Horioka <mhorioka@gmail.com>
2022-01-03 22:10:08 +0900

    Initial commit

---
+++ src/main/java/com/example/hellospringbootremotedev/HelloSpringBootRemoteDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellospringbootremotedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootRemoteDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBootRemoteDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellospringbootremotedev/HelloSpringBootRemoteDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellospringbootremotedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootRemoteDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
